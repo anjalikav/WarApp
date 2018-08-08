f2238b05c3ce94b0715438a0fc0d8141681c1987
kavthkr2 <anjaliatulkavthekar@gmail.com>
2018-08-08 15:07:49 -0500

    First commit

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
 
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +29,40 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    var playerScore = 0
+    var cpuScore = 0
+    
+    
+    @IBAction func dealTapped(sender: AnyObject) {
+        
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        
+        //print("left random number is: \(leftRandomNumber)")
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        
+        //print("right random number is: \(rightRandomNumber)")
+        
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+        
+        
+        
+        if leftRandomNumber > rightRandomNumber {
+            playerScore++
+            leftScoreLabel.text = String(playerScore)
+        }
+        else if rightRandomNumber > leftRandomNumber {
+            cpuScore++
+            rightScoreLabel.text = String(cpuScore)
+            
+        }
+        else {
+            
+        }
+        
+        
     }
 
 
